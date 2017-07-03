85e5ffbe2356b33b7c0c0b7a13d2c4f4619dad9f
Alexander Loechel <Alexander.Loechel@lmu.de>
2017-07-03 11:42:32 +0300

    fix isort and flake8 errors

--- src/sphinxtheme/plone/__init__.py
+++ src/sphinxtheme/plone/__init__.py
@@ -2,15 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import os
-from sphinx.errors import ThemeError
-
 
 
 def get_html_theme_path():
     """Return list of HTML theme paths."""
     cur_dir = os.path.abspath(os.path.dirname(__file__))
     return [cur_dir]
-    
+
+
 if __name__ == '__main__':
     print get_html_theme_path()
-
